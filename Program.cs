fe6a71d53e1a77e344cc2835be01342916c87f22
Daniel <donybof1@gmail.com>
2024-09-20 07:40:48 +0300

    Finished Mission

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main()
+    {
+
+    }
+    public static Stack<int> ReverseStack(Stack<int> Stack)
+    {
+        Stack<int> Reversed = new Stack<int>();
+        while (Stack.Count > 0)
+        {
+            Reversed.Push(Stack.Pop());
+        }
+        return Reversed;
+    }
+    public static Stack<int> MergeStack(Stack<int> stack1, Stack<int> stack2)
+    {
+        Stack<int> Merged = new();
+        stack1 = ReverseStack(stack1);
+        while (stack1.Count > 0)
+        {
+            Merged.Push(stack1.Pop());
+        }
+        stack2 = ReverseStack(stack2);
+        while (stack2.Count > 0)
+        {
+            Merged.Push(stack2.Pop());
+        }
+        return Merged;
+    }
+}
